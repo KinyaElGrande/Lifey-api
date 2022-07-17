b97527bfc90a9e07a5dc6a5e67d921fdc1b09d71
kinyaelgrande <kinya.francis.mumbi@gmail.com>
2022-07-17 18:35:47 +0300

    Initial setup, model and migrations

---
+++ app/Models/Task.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Task extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'label_id',
+    ];
+
+    protected $casts = [
+      'due_date' => 'date:Y-m-d',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function labels()
+    {
+        return $this->hasMany(Label::class);
+    }
+}
